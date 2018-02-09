1c2e368e1c89074bc04d1d52cea351a0c7aa4dc8
yunshu <yunshu_luo@163.com>
2018-02-09 16:40:38 +0800

    添加主题接口控制器和模型

---
+++ appserver/application/api/controller/v1/Theme.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * author: yunshu
+ * createTime: 18/2/9 16:37
+ * description:
+ */
+
+namespace app\api\controller\v1;
+
+
+use app\api\controller\Base;
+
+class Theme extends Base
+{
+
+}
---
+++ appserver/application/api/model/Theme.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * author: yunshu
+ * createTime: 18/2/9 16:38
+ * description:
+ */
+
+namespace app\api\model;
+
+
+class Theme extends Base
+{
+
+}
